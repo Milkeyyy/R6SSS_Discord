41d51c2006ad87222efbe301c2defdc1ba3eb4c2
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2022-12-12 21:22:16 +0900

    とりあえずステータスを取得して送信できるようになった

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import argparse
 import asyncio
 import logging
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from time import sleep
 import sys
 
 import discord
@@ -16,6 +24,9 @@ bot_name = "R6ServerStatusBot"
 # Botのバージョン
 bot_version = "1.0.0"
 
+# サーバーステータスページのURL
+status_url = "https://www.ubisoft.com/ja-jp/game/rainbow-six/siege/status"
+
 # 引数ぱーさー
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -24,7 +35,7 @@ parser.add_argument(
 	type=str,
 	required=True
 )
-parser.add_argument(
+""" parser.add_argument(
 	"--ubi_email",
 	help="Ubisoftアカウントのメールアドレス",
 	type=str,
@@ -35,7 +46,7 @@ parser.add_argument(
 	help="Ubisoftアカウントのパスワード",
 	type=str,
 	required=True
-)
+) """
 args = parser.parse_args()
 
 # トークンが指定されていない場合はエラーを発生させる
@@ -63,15 +74,38 @@ async def on_ready():
 
 # 関数
 async def getserverstatus():
-	auth = Auth(args.ubi_email, args.ubi_password)
-	status = 
+	chrome_options = Options()
+	#chrome_options.add_argument('--no-sandbox')
+	#chrome_options.add_argument('--disable-dev-shm-usage')
+
+	# ウィンドウを表示しない
+	chrome_options.add_argument("--headless")
+
+	driver = webdriver.Chrome("chromedriver_win32\chromedriver", options=chrome_options)
+
+	driver.get(status_url)
+
+	wait = WebDriverWait(driver, 10)
+	selector = '/html/body/div[1]/main/div/div/div[1]/div/ul[1]/li[1]/span/small'
+	wait.until(EC.visibility_of_element_located((By.XPATH, selector)))
+
+	text = driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div/div[1]/div/ul[1]/li[1]/span/small').text
+
+	return text
 
 
 # コマンド
 @client.slash_command()
 async def status(ctx):
 	logging.info(f"コマンド実行: status / 実行者: {ctx.user}")
-	await ctx.respond(embed=embed)
+
+	await ctx.defer()
+
+	# サーバーステータスを取得
+	text = await getserverstatus()
+
+	print(f"Status: {text}")
+	await ctx.followup.send(content=f"Status: {text}")
 
 @client.slash_command()
 async def create(ctx):
@@ -79,11 +113,12 @@ async def create(ctx):
 
 @client.slash_command()
 async def ping(ctx):
+	logging.info(f"コマンド実行: ping / 実行者: {ctx.user}")
 	raw_ping = client.latency
 	ping = round(raw_ping * 1000)
 	ping_embed = discord.Embed(title="Pong!",description=f"Latency: **`{ping}`** ms",color=discord.Colour.from_rgb(79,168,254))
 	await ctx.respond(embed=ping_embed)
-	logging.info(f"コマンド実行: ping / 実行者: {ctx.user}")
+
 
 
 # ログイン
