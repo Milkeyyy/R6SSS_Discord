6371145e224e0e59cb8a65df7a7cc4dd1e442f6e
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2022-12-19 03:21:37 +0900

    Update heartbeat.py

--- src/heartbeat.py
+++ src/heartbeat.py
@@ -21,7 +21,7 @@ def loadKeys():
 		logging.error("Cronitorのキーが指定されていません")
 		sys.exit(1)
 
-	logging.info(f"Cronitor API/Heartbeat Keys has been loaded: {v[0]}/{v[1]/v[2]}")
+	logging.info(f"Cronitor API/Heartbeat Keys has been loaded: {v[0]}/{v[1]}/{v[2]}")
 
 	cronitor.api_key = v[0]
 	heartbeat = cronitor.Monitor(v[1])
