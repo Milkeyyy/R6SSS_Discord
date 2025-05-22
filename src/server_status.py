c024a65741b2f9d3aec2c3f1ae650e1d8f9d13e1
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2025-05-22 16:56:16 +0900

    Revert revert

--- src/server_status.py
+++ src/server_status.py
@@ -11,10 +11,9 @@ API_URL = "https://api-r6sss.milkeyyy.com/v2/status"
 class ServerStatusManager:
 	"""サーバーステータスを管理するクラス"""
 
-	def __init__(self) -> None:
-		self.data = []
-		self.updated_at = 0
-		self.indicator = status_indicator.Unknown # テキストチャンネルの名前に表示するステータスインジケーター(絵文字)
+	data = []
+	updated_at = 0
+	indicator = status_indicator.Unknown # テキストチャンネルの名前に表示するステータスインジケーター(絵文字)
 
 	@classmethod
 	async def get(cls) -> list[r6sss.Status] | None:
