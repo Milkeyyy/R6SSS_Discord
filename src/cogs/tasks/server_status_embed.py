bb85f3e90925636474a70e0cb8058e21d104b393
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2025-06-03 14:28:34 +0900

    list[list] を list へ修正

--- src/cogs/tasks/server_status_embed.py
+++ src/cogs/tasks/server_status_embed.py
@@ -141,7 +141,7 @@ class ServerStatusEmbedManager(commands.Cog):
 
 						try:
 							# 設定言語のサーバーステータスの埋め込みメッセージを取得
-							target_embeds = [status_embeds.get(lang)]
+							target_embeds = status_embeds.get(lang)
 							if target_embeds[0] is None:
 								logger.error("サーバーステータスメッセージの取得失敗: 言語 %s の埋め込みメッセージが存在しません", lang)
 								target_embeds = None
