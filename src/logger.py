a702e7c29a2b6f1cec6e644c79db403ec57b9db0
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2025-12-29 13:10:33 +0900

    fix: ロガーの挙動を修正

--- src/logger.py
+++ src/logger.py
@@ -3,12 +3,13 @@ import logging.handlers
 from pathlib import Path
 
 discord_logger = logging.getLogger("discord")
-discord_logger.setLevel(logging.ERROR)
+discord_logger.setLevel(logging.INFO)
 logger = logging.getLogger("discord.app")
 logger.setLevel(logging.DEBUG)
 
 stream_formatter = logging.Formatter(
 	fmt="[%(asctime)s.%(msecs)03d] %(levelname)s [%(thread)d] %(message)s",
+	datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 stream_handler = logging.StreamHandler()
@@ -19,13 +20,11 @@ if not log_file_path.exists():
 	log_file_path.parent.mkdir(exist_ok=True)
 	log_file_path.touch(exist_ok=True)
 
-rotating_handler = logging.handlers.RotatingFileHandler(r"./logs/app.log", mode="a", maxBytes=100 * 1024, backupCount=10, encoding="utf-8")
+rotating_handler = logging.handlers.RotatingFileHandler(r"./logs/app.log", mode="a", maxBytes=1000 * 1024, backupCount=10, encoding="utf-8")
 rotating_handler.setLevel(logging.DEBUG)
 rotating_handler.setFormatter(stream_formatter)
 
 discord_logger.addHandler(rotating_handler)
 logger.addHandler(rotating_handler)
 logger.addHandler(stream_handler)
-
-# フォルダーを作成する
-Path("./logs").mkdir(exist_ok=True)
+logger.propagate = False
